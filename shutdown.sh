ce523a0459daa3a9deef96b3ccd7e59fe71cf491
Caelin Sutch <caelinsutch@gmail.com>
2020-01-25 10:57:39 -0800

    Updated shutdown script

--- shutdown.sh
+++ shutdown.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 USERNAME=jhsrobo
 PASSWORD=JHSRobo
-sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -l $USERNAME -t bottomside 'echo "JHSRobo" | sudo -S shutdown -h now'
+sshpass -p $PASSWORD ssh -t -t -o StrictHostKeyChecking=no -l $USERNAME -t bottomside 'echo "JHSRobo" | sudo -S shutdown -h now'
+
+exit
